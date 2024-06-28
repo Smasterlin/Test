787fa7eb6090318a09829db9c344107e8531391c
Smasterlin <topmasters@163.com>
2024-06-28 09:21:14 +0800

    第三次提交

--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -7,7 +7,7 @@ public class Test2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("第二次进行测试");
+        Debug.Log("第三次在家克隆下项目之后，然后推送仓库");
     }
 
     // Update is called once per frame
