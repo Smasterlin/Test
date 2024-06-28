c58c87ba0d305825df7e2bf6e1b3e0a73c589f30
Smasterlin <topmasters@163.com>
2024-06-28 08:33:07 +0800

    第二次提交

--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -7,7 +7,7 @@ public class Test2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("第一次提交");
+        Debug.Log("第二次进行测试");
     }
 
     // Update is called once per frame
